4281c28377f2a4e02b57d3ec082f0349adef780c
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2025-07-18 13:59:18 -0300

    feat: loading envs

--- src/main/config.go
+++ src/main/config.go
@@ -1,12 +1,7 @@
 package config
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -18,10 +13,11 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	err := godotenv.Load()
-	listFiles()
-	if err != nil {
-		return nil, err
+	if os.Getenv("APP_ENV") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &Config{
@@ -29,30 +25,4 @@ func LoadConfig() (*Config, error) {
 		DB_HOST: os.Getenv("DB_HOST"),
 		DB_PORT: os.Getenv("DB_PORT"),
 	}, nil
-}
-
-func listFiles() {
-	// Listar arquivos em um diretório específico
-	diretorio := "." // Diretório atual
-	arquivos, err := ioutil.ReadDir(diretorio)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Arquivos no diretório:", diretorio)
-	for _, arquivo := range arquivos {
-		fmt.Println(arquivo.Name(), arquivo.IsDir())
-	}
-
-	// Listar arquivos recursivamente
-	fmt.Println("\nArquivos recursivamente (usando filepath.Walk):")
-	filepath.Walk(diretorio, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !strings.Contains(path, ".git") {
-		fmt.Println(path)
-		}
-		return nil
-	})
 }
